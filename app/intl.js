b5f9744a366bfa0d4ca166a9df5269b31f68f8e6
Nicolas Venne <nvenne@142-51-0-242.laurentian.ca>
2018-11-29 14:09:33 -0500

    Finished 4 steps of form

--- app/intl.js
+++ app/intl.js
@@ -3,15 +3,27 @@ import {MDCTextField} from '@material/textfield';
 import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
 import {MDCSelect} from '@material/select';
 import {MDCSnackbar} from '@material/snackbar';
+import {MDCDialog} from '@material/dialog';
+import {MDCMenu} from '@material/menu';
 
 const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-const dataObj = {
-  message: "You have not completed the previous step",
-  actionText: 'Undo',
-  actionHandler: function () {
-    console.log('my cool function');
+function showMsg(msg) {
+  const dataObj = {
+    message: msg
+  };
+  snackbar.show(dataObj);
+}
+
+function showMsgUndo(msg, callback) {
+  const dataObj = {
+    message: msg,
+    actionText: 'Undo',
+    actionHandler: function () {
+      callback()
+    }
   }
-};
+  snackbar.show(dataObj);
+}
 let currentTab = 0;
 var displaySize;
 resize();
@@ -24,8 +36,18 @@ function resize() {
     td.style.width = displaySize + "px";
   });
   document.querySelector('table').style.width = displaySize * 5;
-  console.log(currentTab);
   document.querySelector('.move-row').style.transform = "translateX(" + displaySize * (currentTab * -1) + "px)";
+  var sectionHeight;
+
+  document.querySelectorAll('.animated-section').forEach(step => {
+    if(step.dataset.step == currentTab) {
+      sectionHeight = step.offsetHeight;
+      var tableHeight = document.querySelector('.table-height').offsetHeight;
+      document.querySelector('.formDisplay').style.height = sectionHeight + 80 + "px";
+      document.querySelector('.actionButtons').style.position = "relative";
+      document.querySelector('.actionButtons').style.bottom = tableHeight - sectionHeight - 24 + "px"
+    }
+  })
 }
 
 
@@ -38,6 +60,16 @@ tabBar.listen('MDCTabBar:activated', function(event) {
   currentTab = nextTab;
   let diff = (nextTab) * -1;
   document.querySelector('.move-row').style.transform = "translateX(" + displaySize * diff + "px)";
+  var sectionHeight;
+  document.querySelectorAll('.animated-section').forEach(step => {
+    if(step.dataset.step == nextTab) {
+      sectionHeight = step.offsetHeight;
+      var tableHeight = document.querySelector('.table-height').offsetHeight;
+      document.querySelector('.formDisplay').style.height = sectionHeight + 80 + "px";
+      document.querySelector('.actionButtons').style.position = "relative";
+      document.querySelector('.actionButtons').style.bottom = tableHeight - sectionHeight - 24 + "px"
+    }
+  })
   document.querySelectorAll('.step').forEach(step => {
     if(step.dataset.step == nextTab) {
       step.classList.remove("hidden");
@@ -46,6 +78,213 @@ tabBar.listen('MDCTabBar:activated', function(event) {
     }
   })
 });
+
+
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+
+document.querySelector('.openDialog').addEventListener('click', function() {
+  dialog.open();
+})
+
+dialog.listen('MDCDialog:closed', function(event) {
+  if(event.detail.action == "yes") {
+    document.querySelector('.openDialog').innerHTML = "Requirements met";
+    document.querySelector('.requirementsMetYes').checked = true;
+    document.querySelectorAll('.requirementsMet').forEach(div => {
+      div.classList.remove("hidden");
+      div.firstChild.nextSibling.classList.remove("mdc-select--disabled");
+    });
+    document.querySelectorAll('.requirementsNotMet').forEach(div => {
+      div.classList.add("hidden");
+      div.firstChild.nextSibling.classList.add("mdc-select--disabled");
+    });
+    document.querySelectorAll('.requirementsNotMetInput').forEach(div => {
+      div.disabled = true;
+
+    });
+    document.querySelectorAll('.requirementsMetInput').forEach(div => {
+      div.disabled = false;
+    });
+    console.log("i an1");
+    showMsg("Requirements met");
+  } else {
+    document.querySelector('.openDialog').innerHTML = "Requirements not met";
+    document.querySelector('.requirementsMetNo').checked = true;
+    document.querySelectorAll('.requirementsMet').forEach(div => {
+      div.classList.add("hidden");
+      div.firstChild.nextSibling.classList.add("mdc-select--disabled");
+    });
+    document.querySelectorAll('.requirementsNotMet').forEach(div => {
+      div.classList.remove("hidden");
+      div.firstChild.nextSibling.classList.remove("mdc-select--disabled");
+    });
+    document.querySelectorAll('.requirementsNotMetInput').forEach(div => {
+      div.disabled = false
+    });
+    document.querySelectorAll('.requirementsMetInput').forEach(div => {
+      div.disabled = true;
+    });
+    console.log("i an");
+    showMsg("Requirements not met");
+  }
+  resize();
+});
+
+var schoolIndex = 1;
+document.querySelector('.addHighSchool').addEventListener('click', function(event) {
+  console.log(event);
+  let button = event.target;
+  if(schoolIndex == 3) {
+    showMsg("Max 3 high schools");
+    return;
+  }
+  schoolIndex++;
+  console.log(schoolIndex);
+  document.querySelector('.highschool--' + schoolIndex).style.display = "block";
+  setTimeout(function() {
+    document.querySelector('.highschool--' + schoolIndex).style.transform = "scale(1)";
+  }, 0);
+  document.querySelector('.highschool--' + schoolIndex).querySelectorAll('.initSchoolInputs').forEach(input => {
+    input.disabled = false;
+  });
+  showMsgUndo("Added Secondary School", function() {
+    let tmp = document.querySelector('.addHighSchool');
+    if(schoolIndex == 1) {
+      showMsg("Minimum 1 high school");
+      return;
+    }
+    document.querySelector('.highschool--' + schoolIndex).style.transform = "scale(0)";
+    setTimeout(function() {
+      document.querySelector('.highschool--' + schoolIndex).style.display = "none";
+      schoolIndex--;
+      resize();
+    }, 499)
+    document.querySelector('.highschool--' + schoolIndex).querySelectorAll('.initSchoolInputs').forEach(input => {
+      input.disabled = true;
+    });
+    showMsg("Undo");
+  });
+  resize();
+
+});
+
+document.querySelector('.removeHighSchool').addEventListener('click', function(event){
+      let tmp = document.querySelector('.addHighSchool');
+      if(schoolIndex == 1) {
+        showMsg("Minimum 1 high school");
+        return;
+      }
+      document.querySelector('.highschool--' + schoolIndex).style.transform = "scale(0)";
+      setTimeout(function() {
+        document.querySelector('.highschool--' + schoolIndex).style.display = "none";
+        schoolIndex--;
+        resize();
+      }, 499)
+      document.querySelector('.highschool--' + schoolIndex).querySelectorAll('.initSchoolInputs').forEach(input => {
+        input.disabled = true;
+      });
+      showMsg("Removed Secondary School");
+
+});
+
+var postSchoolIndex = 1;
+document.querySelector('.addPostsecondarySchool').addEventListener('click', function(event) {
+  let button = event.target;
+  if(postSchoolIndex == 3) {
+    showMsg("Max 3 postsecondary schools");
+    return;
+  }
+  postSchoolIndex++;
+  console.log(postSchoolIndex);
+  document.querySelector('.postsecondary--' + postSchoolIndex).style.display = "block";
+  setTimeout(function() {
+    document.querySelector('.postsecondary--' + postSchoolIndex).style.transform = "scale(1)";
+  }, 0);
+  document.querySelector('.postsecondary--' + postSchoolIndex).querySelectorAll('.initSchoolInputs').forEach(input => {
+    input.disabled = false;
+  });
+  showMsgUndo("Added Postsecondary School", function() {
+    document.querySelector('.postsecondary--' + postSchoolIndex).style.transform = "scale(0)";
+    setTimeout(function() {
+      document.querySelector('.postsecondary--' + postSchoolIndex).style.display = "none";
+      postSchoolIndex--;
+      resize();
+    }, 499)
+    document.querySelector('.postsecondary--' + postSchoolIndex).querySelectorAll('.initSchoolInputs').forEach(input => {
+      input.disabled = true;
+    });
+    showMsg("Undo");
+  });
+  resize();
+
+});
+
+document.querySelector('.removePostsecondarySchool').addEventListener('click', function(event){
+      let tmp = document.querySelector('.addHighSchool');
+      if(postSchoolIndex == 1) {
+        showMsg("Minimum 1 Postsecondary school");
+        return;
+      }
+      document.querySelector('.postsecondary--' + postSchoolIndex).style.transform = "scale(0)";
+      setTimeout(function() {
+        document.querySelector('.postsecondary--' + postSchoolIndex).style.display = "none";
+        postSchoolIndex--;
+        resize();
+      }, 499)
+      document.querySelector('.postsecondary--' + postSchoolIndex).querySelectorAll('.initSchoolInputs').forEach(input => {
+        input.disabled = true;
+      });
+      showMsg("Removed Postsecondary School");
+
+});
+
+
+
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+document.querySelector('.languageButton').addEventListener('click', function() {
+  menu.open = true;
+})
+
+menu.listen('MDCMenu:selected', function(event) {
+  switch(event.detail.index) {
+    case 0:
+      window.location.pathname = "international/en";
+      break;
+    case 1:
+      window.location.pathname = "international/fr";
+      break;
+    default:
+      break;
+  }
+})
+
+
+document.querySelector('.attened-ps__yes').addEventListener('click', function() {
+  document.querySelectorAll('.psButton').forEach(but => {
+    but.style.transform = "scale(1)";
+  });
+  document.querySelector('.ps-section').style.display = "block";
+  for(let i = 1; i <= postSchoolIndex; i++ ) {
+    document.querySelector('.postsecondary--' + i).querySelectorAll('.initSchoolInputs').forEach(input => {
+      input.disabled = false;
+    });
+  }
+  resize();
+})
+
+document.querySelector('.attened-ps__no').addEventListener('click', function() {
+  document.querySelector('.ps-section').style.display = "none";
+  document.querySelectorAll('.psButton').forEach(but => {
+    but.style.transform = "scale(0)";
+  });
+
+  document.querySelectorAll('.initSchoolInputs').forEach(input => {
+    input.disabled = true;
+  });
+
+  resize();
+})
+
 
 // function checkTabs() {
 //   document.querySelectorAll('.mdc-tab').forEach(tab => {
@@ -64,6 +303,7 @@ function checkValidity(formID, callback) {
   let form = document.forms['form-step-' + formID];
   for(let i = 0; i < form.elements.length; i++) {
     let input = form.elements.item(i);
+    checkValidityInput(input);
     input.addEventListener("change", function() {checkValidityInput(input)});
 
   }
@@ -73,13 +313,14 @@ function checkValidity(formID, callback) {
 }
 
 function checkValidityInput(input) {
-  console.log("typeing");
   if(input.required && input.value == "") {
     input.setCustomValidity(input.nextSibling.nextSibling.innerHTML + " is required");
   } else {
     input.setCustomValidity("");
+
   }
 }
+
 
 document.querySelectorAll('.form-submit').forEach(button => {
   button.addEventListener('click', function() {
@@ -102,3 +343,7 @@ document.querySelectorAll('.mdc-text-field').forEach(field => {
 document.querySelectorAll('.mdc-text-field-helper-text').forEach(helper => {
   new MDCTextFieldHelperText(helper);
 })
+
+document.querySelectorAll('.initSchoolInputs').forEach(input => {
+  input.disabled = true;
+});
